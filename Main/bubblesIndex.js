4973a2ab7c431fc036e8950803140cafde5a4bd2
Anthony Chapman <micantcha@gmail.com>
2020-12-09 20:22:32 +0200

    Update main page

---
+++ Main/bubblesIndex.js
@@ -0,0 +1,10 @@
+function showName(e){
+    var firstName= document.getElementById("firstname").value;
+    drawText(firstName);        
+}
+
+
+// The code below will allow you to interact with the letters
+bounceBubbles();
+
+
